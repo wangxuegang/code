960675c8008d7c62f7384520038dde2a39e64880
wangxuegang <15510235102@163.com>
2018-12-24 17:35:15 +0800

    ssm

---
+++ ssm/src/main/java/com/wangxuegang/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.wangxuegang.dao;
+
+import com.wangxuegang.enitiy.UserEo;
+
+/**
+ * 
+ * @类描述：UserDao
+ * @项目名称：ssm
+ * @包名： com.wangxuegang.dao
+ * @类名称：UserDao
+ * @创建人：wangxuegang
+ * @创建时间：2018年12月24日上午11:06:29
+ * @mail 15510235102@163.com
+ * @version v1.0
+ */
+public interface UserDao {
+	
+	void insertUser(UserEo userEo);
+	
+}
---
+++ ssm/src/main/java/com/wangxuegang/service/UserService.java
@@ -0,0 +1,19 @@
+package com.wangxuegang.service;
+
+import com.wangxuegang.model.UserVo;
+
+/**
+ * 
+ * @类描述：UserService
+ * @项目名称：ssm
+ * @包名： com.wangxuegang.service
+ * @类名称：UserService
+ * @创建人：wangxuegang
+ * @创建时间：2018年12月24日下午3:04:23
+ * @mail 15510235102@163.com
+ * @version v1.0
+ */
+public interface UserService {
+	
+	void saveUser(UserVo userVo);
+}
