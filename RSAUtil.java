269206c998ce4b1a9bc1a22985813ba1a6349853
晴空万里 <15510235102@163.com>
2020-04-24 16:11:42 +0800

    Update RSAUtil.java

--- RSAUtil.java
+++ RSAUtil.java
@@ -16,6 +16,8 @@ import javax.crypto.Cipher;
 
 import org.apache.commons.codec.binary.Base64;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -114,7 +116,7 @@ public class RSAUtil {
         return outStr;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlipayApiException {
     	
     	
 //		前端用crypto-js进行加密，npm i jsencrypt
@@ -130,5 +132,15 @@ public class RSAUtil {
     	
 //    	String data = RSAUtil.encrypt("test", "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgA6IoAudhnqPhOjGxsU/54v3XtEmquY08iURxmBw+ocjrVhpSpiFZ3ueYOccoqc2PhHXXymrftd5EpAgWp3upmWHCnajFSx5oy8SwBDFQidZjAeinnBh+H6FvjP8wufK4YE17AiH5kHvsNeRp/6aj4J8CqK3YWoSqXvURWAeHUQIDAQAB");
 //    	System.out.println(RSAUtil.decrypt(data, "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKADoigC52Geo+E6MbGxT/ni/de0Saq5jTyJRHGYHD6hyOtWGlKmIVne55g5xyipzY+EddfKat+13kSkCBane6mZYcKdqMVLHmjLxLAEMVCJ1mMB6KecGH4foW+M/zC58rhgTXsCIfmQe+w15Gn/pqPgnwKordhahKpe9RFYB4dRAgMBAAECgYAU0cQ0boRKeEicUmUK2KYuPjGG8kcvdmsA+F82ZHMwVv58WDyeik0Gk3EQplvaV5WOWc63X/fd2Zzs02bWA76cEjFAtGcEVj1pWVEbVx0fF60P8J2PwADeNccpj55D3Z/Agy4af/FTTJhsvUr2d1cuzjODFMeWAAg9QPrAQKn8wQJBAOx64NABEDi4AJrZEcji/rxZzTN9LpwdH9KVh57uwA9PNrjHeYrLJNUv5Y++B6x7e/Su+7FU5ppnBGxGn1xgJ1kCQQCtOO+furggXjF3+C19ypsIec382Wc9Y5G9YPLst+t9tPH/81tC7O9kz6Iz9EENE7vzMYcAomKs/APC02VNGNi5AkBpzNwSE7e3OJOEtANh9jTz/dVx6NrWm60mISJJOBYTg2Q/LXeyYgq7mq9BLUrvn1uo8DTJdOurPtXav4oC4T4ZAkBQeaYhsA6AwyH6WWtRCIKUInqHYL0s8QgxUkwpm5ylLx5KydV5NzhUvn3d4zkhiSyFZFoS+l4bfY2Fws5KTiPJAkA9LpzNnYAo0E4hYySaIow7c/UH3azJoGZH0CoHzZwg0yMxHUFLc0jrkyRXjODCkFtKeywz7NgwNGw0dhaGrfxd"));
+    	
+    	String privateKey = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCZ2k6xlg5U1RjYLb2tfAVsKZ/rdJU3vDcuJT5F43zthQgwt6EFEshgXR7DDaBT3jqLfl5H6uMluwJ3v+zW9gu9U+L09hgrL+HZk9qFzGHnGNQ2HAt7dcg1PBzYhwflA9bwweKiXE4RkK2537U0+tMic9q+9Ykze6QX7tBklmhZNAvV0cOK8Wgb4NIonKKDkC0TiFcHZxfnBMp3Hc9QasEpLiC0rS9K2Mq4+DLTJe+GW1mWdaBw+fHvMzcTb9Ph+n5H2g71jYMMWNKjtOHZE/XaVPSdlcT1tPMpQqQFOj/VmLOv0uKd48mgVvHFOPAg7/ncqimdPEZu7XhAdLca7TmlAgMBAAECggEAWazEB4B2E/4tP/vRPeg25OeSHdRTESx0YUI1/Nhuvaa3smWlxeY/wuMkBf7QP3IdX6clXvfKs/g/pPrKGjVJfG4DsFIsnieHlaE3UpthSSjQsEVCcBCjxFuoWJDECjlls+jep1Hz8wsIJ3n8DchQ/hjXHEzTTp23dHevIaIFalAipRAP6QrwEEDLOZgyPdkTpbJLth3dU6a3sx1gglkPzkB/wx4reajCWRG3+I9pNV/TIfksOiKXPsMlU0TUV/TX85AXGNFZ29e8X2y5ts5//r6eedFNdAKmx8FSQ1iTlyOxMDmEc0l1oztTSdOcjchO2gXSOTTsUQRZeqV5obE9AQKBgQDXNUIj7Kb84+Zyl1UXXq38YyFOyiqJPbav1sTr4I8TbxDPY2NaSqapfbRfY97Z6+mvrefTSa5R8H/k3FXb2bYMj+WSd4OxUJGHF8fdc8ddAZpkD5IkejqSnJyvCAqczazLO3SW7b1bSW+NpObITenZ+NyE4ll4F8JS04sJFy2d5QKBgQC3A9lvzPgpL2FxZfMptrtFE8os0j769v3O24eiuFO0s5CbkZlMIB1atqnriEQTrLS+qkJWn9WQgTwF1Sk88OrPGRVJc9Qezxozci8MbXJ04G6BTHPVGHxgK9/XADyIC3+ULIjHu2Vw+2YCqx8XqDUXxZZaOEZPXp+EyNxva1FwwQKBgFdpF2s2BQF0o7ZphzNNzodT0ESLUsmZmmlDZ4qTFCoFk6NYsBMNfTIcj7Wq/+otqyYjEaU9KSO49omDgRAiFpJ1o/5gssnDesiPPSvSiehZ7Va1CFvulXnVSMLDFmRoGcYWaR2ghKfeZQP4NZCoLNLPBOqocUSZCeHQcImJs4qNAoGATvkiQtQ34c3IRp6vpzhq5wv7Ggw/kRwzuaeRh+3ycO9rXzYc6HSTNinMeQ58EeFcvUnLBoZCKfEabYnmGZ38NRGl1eRdjE3iCIElPD+ePED8bl8HPNUsFnPYS3aWqD4N92qgO0/qHi7dfhlJeXPkNTXWA2h1LpquSyZBuPKeWgECgYBYdrmRbSk/ZMwQfrK/rTBOBBBanZ3Xo0T2Byoh/K49iJLc6jyvJqe4W+69rvRCGxaR+mK7xjyfZL9lzvxQaU9Vc3JbPd6wk0tTjqnfSsOw9Az0Qbm5+Y6YsuXczC8gHRCEyxJJaAw41Eb+/WC4GGRC1qzlHmOvV6DSIBbM2CQeKw==";
+    	String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmdpOsZYOVNUY2C29rXwFbCmf63SVN7w3LiU+ReN87YUIMLehBRLIYF0eww2gU946i35eR+rjJbsCd7/s1vYLvVPi9PYYKy/h2ZPahcxh5xjUNhwLe3XINTwc2IcH5QPW8MHiolxOEZCtud+1NPrTInPavvWJM3ukF+7QZJZoWTQL1dHDivFoG+DSKJyig5AtE4hXB2cX5wTKdx3PUGrBKS4gtK0vStjKuPgy0yXvhltZlnWgcPnx7zM3E2/T4fp+R9oO9Y2DDFjSo7Th2RP12lT0nZXE9bTzKUKkBTo/1Zizr9LinePJoFbxxTjwIO/53KopnTxGbu14QHS3Gu05pQIDAQAB";
+    	String data = AlipaySignature.encrypt("test", publicKey, "UTF-8", "RSA2");
+    	String value = AlipaySignature.decrypt(data, privateKey, "UTF-8", "RSA2");
+    	System.out.println(value);
+    	
+    	//验签
+    	System.out.println(AlipaySignature.verify("test", AlipaySignature.rsa256Sign("test", privateKey, "UTF-8"), publicKey, "UTF-8", "RSA2"));
+    	
 	}
 }
